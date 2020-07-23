35f450dbd635594d5817a28ce6618fdfb7289ee9
MrSpooner <spoontamer7@gmail.com>
2020-07-23 19:24:36 +0500

    Slider added

--- app/plugins/script.js
+++ app/plugins/script.js
@@ -1,38 +1,36 @@
 let array = [];
-maxHeight = 350;
-
+const maxHeight = 350;
+let columnQuantity;
 const field = document.querySelector(".field");
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
-
 slider.oninput = function () {
     output.innerHTML = this.value;
 }
 
-
-
-function textNodeDelete(array) {
-    const columnField = document.querySelectorAll(".field__column");
-    let i = 0;
-    for (i = 0; i < array.length; i++) {
-        columnField[i].parentNode.removeChild(columnField[i].nextSibling);
-    }
-    columnField[0].parentNode.removeChild(columnField[0].previousSibling);
-}
-
 function calcArray() {
+    field.querySelectorAll('*').forEach(n => n.remove());
     array = [];
-    for (var i = 0; i < 10; i++) {
+    columnQuantity = slider.value;
+    for (var i = 0; i < columnQuantity; i++) {
         array.push(maxHeight);
     }
     for (i = 0; i < array.length; i++) {
         array[i] = array[i] * ((i + 1) * ((0.6 / array.length)));
     }
+    generateColumns(array);
     shuffle(array);
-    textNodeDelete(array);
     bubbleSort(array);
+}
+
+function generateColumns(array) {
+    for (let i = 0; i < array.length; i++) {
+        fieldCol = document.createElement('div');
+        fieldCol.setAttribute('class', 'field__column');
+        field.appendChild(fieldCol);
+    }
 }
 
 function shuffle(array) {
