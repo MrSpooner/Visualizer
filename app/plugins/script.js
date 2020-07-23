a98b183b0a9f6641c46e9d114b132c435bf88d6e
MrSpooner <spoontamer7@gmail.com>
2020-07-23 21:11:08 +0500

    Beta
    Works fine, no animations tho

--- app/plugins/script.js
+++ app/plugins/script.js
@@ -1,19 +1,32 @@
+'use strict'
 let array = [];
 const maxHeight = 350;
-let columnQuantity;
+let columnQuantity = 20;
+let time = 300;
 const field = document.querySelector(".field");
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
+var slider = document.querySelector("#myRange");
+var output = document.querySelector("#demo");
+var sliderTime = document.querySelector("#myTime");
+var outputTime = document.querySelector("#time");
+
+var form = document.querySelector('.slidecontainer');
+form.addEventListener('change', function () {
+    output.innerHTML = slider.value;
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+    }
+    outputTime.innerHTML = sliderTime.value;
+    sliderTime.oninput = function () {
+        outputTime.innerHTML = this.value;
+    }
+});
 
 function calcArray() {
     field.querySelectorAll('*').forEach(n => n.remove());
     array = [];
     columnQuantity = slider.value;
+    time = sliderTime.value;
     for (var i = 0; i < columnQuantity; i++) {
         array.push(maxHeight);
     }
@@ -22,12 +35,16 @@ function calcArray() {
     }
     generateColumns(array);
     shuffle(array);
+    if (timerId) {
+        clearTimeout(timerId);
+    }
     bubbleSort(array);
+
 }
 
 function generateColumns(array) {
     for (let i = 0; i < array.length; i++) {
-        fieldCol = document.createElement('div');
+        let fieldCol = document.createElement('div');
         fieldCol.setAttribute('class', 'field__column');
         field.appendChild(fieldCol);
     }
@@ -54,22 +71,34 @@ function showArray(array) {
 }
 
 function swap(num1, num2) {
+    field.childNodes[num1].style.background = 'green';
+    field.childNodes[num2].style.background = 'green';
+    // field.childNodes[num1].setAttribute('style', `transform: translate(50px, 54px)`);
     let clone = field.childNodes[num1].cloneNode(true);
     field.childNodes[num1].replaceWith(field.childNodes[num2]);
     field.childNodes[num2 - 1].after(clone);
 }
 
-function bubbleSort(array) {
-    let swapp, temp, i;
+let timerId;
+
+function timer(ms) {
+    return new Promise(res => { timerId = setTimeout(res, ms) });
+}
+
+async function bubbleSort(array) {
+    let swapp, temp;
     let n = array.length - 1, x = array;
     do {
         swapp = false;
-        for (i = 0; i < n; i++) {
+        for (let i = 0; i < n; i++) {
             if (x[i] > x[i + 1]) {
                 temp = x[i];
                 x[i] = x[i + 1];
                 x[i + 1] = temp;
                 swap(i, i + 1);
+                await timer(time);
+                field.childNodes[i + 1].style.background = '#8458B3';
+                field.childNodes[i].style.background = '#8458B3';
                 swapp = true;
             }
 
